1bbd7502c45c2dafdea015f479ddb2a6de290756
Nik Paro <niklas.paro@gmail.com>
2017-08-12 15:06:24 +0300

    simplified demo that works without a web server, and downloads on reload

--- demo/browser.js
+++ demo/browser.js
@@ -1,15 +1,26 @@
 var PDFDocument = require('../');
-var blobStream = require('blob-stream');
-var ace = require('brace');
-require('brace/mode/javascript');
-require('brace/theme/monokai');
+var saveAs = require('file-saver').saveAs;
 
 var lorem = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam in suscipit purus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Vivamus nec hendrerit felis. Morbi aliquam facilisis risus eu lacinia. Sed eu leo in turpis fringilla hendrerit. Ut nec accumsan nisl. Suspendisse rhoncus nisl posuere tortor tempus et dapibus elit porta. Cras leo neque, elementum a rhoncus ut, vestibulum non nibh. Phasellus pretium justo turpis. Etiam vulputate, odio vitae tincidunt ultricies, eros odio dapibus nisi, ut tincidunt lacus arcu eu elit. Aenean velit erat, vehicula eget lacinia ut, dignissim non tellus. Aliquam nec lacus mi, sed vestibulum nunc. Suspendisse potenti. Curabitur vitae sem turpis. Vestibulum sed neque eget dolor dapibus porttitor at sit amet sem. Fusce a turpis lorem. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae;\nMauris at ante tellus. Vestibulum a metus lectus. Praesent tempor purus a lacus blandit eget gravida ante hendrerit. Cras et eros metus. Sed commodo malesuada eros, vitae interdum augue semper quis. Fusce id magna nunc. Curabitur sollicitudin placerat semper. Cras et mi neque, a dignissim risus. Nulla venenatis porta lacus, vel rhoncus lectus tempor vitae. Duis sagittis venenatis rutrum. Curabitur tempor massa tortor.';
 
-function makePDF(PDFDocument, blobStream, lorem, iframe) {
+function formatDate(date) {
+  return date.getFullYear() + "-" + date.getMonth() + "-" + date.getDate() + "_" + date.getHours() + "-" + date.getMinutes() + "-" + date.getSeconds();
+}
+
+function download() {
+  console.log('download');
+  makePDF(PDFDocument, lorem);
+}
+
+const button = document.getElementById('download-button');
+button.addEventListener('click', download, false);
+
+
+function makePDF(PDFDocument, lorem) {
   // create a document and pipe to a blob
   var doc = new PDFDocument();
-  var stream = doc.pipe(blobStream());
+  const buffers = [];
+  doc.on('data', buffers.push.bind(buffers));
   
   // draw some text
   doc.fontSize(25)
@@ -44,33 +55,13 @@ function makePDF(PDFDocument, blobStream, lorem, iframe) {
        height: 300,
        ellipsis: true
      });
-     
-  // end and display the document in the iframe to the right
-  doc.end();
-  stream.on('finish', function() {
-    iframe.src = stream.toBlobURL('application/pdf');
+
+  // end and download the document
+  doc.on('end', function() {
+    const blob = new Blob(buffers.map(function(buffer) { return buffer.toArrayBuffer() }), {type: 'application/pdf'});
+    // const URL = global.URL || global.webkitURL || global.mozURL;
+    // iframe.src = URL.createObjectURL(pdfData);
+    saveAs(blob, 'test-pdf_'+formatDate(new Date()));
   });
+  doc.end();
 }
-
-var editor = ace.edit('editor');
-editor.setTheme('ace/theme/monokai');
-editor.getSession().setMode('ace/mode/javascript');
-editor.setValue(
-  makePDF
-    .toString()
-    .split('\n').slice(1, -1).join('\n')
-    .replace(/^  /mg, '')
-);
-editor.getSession().getSelection().clearSelection();
-
-var iframe = document.querySelector('iframe');
-makePDF(PDFDocument, blobStream, lorem, iframe);
-
-editor.getSession().on('change', function() {
-  try {
-    var fn = new Function("PDFDocument", "blobStream", "lorem", "iframe", editor.getValue());
-    fn(PDFDocument, blobStream, lorem, iframe);
-  } catch (e) {
-    console.log(e)
-  };
-});
